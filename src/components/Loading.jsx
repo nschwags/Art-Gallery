d5e451baf7a85b99fecdb78df2ebf336e39b0859
nschwags <nschwags@gmail.com>
2025-02-17 01:06:30 -0600

    Created animated loading icon

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import "./loading.css";
+
+function Loading() {
+  return (
+    <div className="dot-container">
+      <div className="dot dot1"></div>
+      <div className="dot dot2"></div>
+      <div className="dot dot3"></div>
+    </div>
+  );
+}
+
+export default Loading;
