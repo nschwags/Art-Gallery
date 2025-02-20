268f81ae84ebf89f60326d2ebf4f0f5bc61daab4
nschwags <nschwags@gmail.com>
2025-02-19 23:40:27 -0600

    Updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,25 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import Exhibitions from "./components/Exhibitions";
 import Explore from "./components/Explore";
+import ExploreDept from "./components/ExploreDept";
+import Artists from "./components/Artists";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="exhibitions" element={<Exhibitions />}></Route>
-        <Route path="explore" element={<Explore />}></Route>
+        <Route path="/explore" element={<Explore />}></Route>
+        <Route path="/explore/:departmentId" element={<ExploreDept />}></Route>
+        <Route path="/artists" element={<Artists />}></Route>
+        <Route path="/explore/search" element={<ExploreDept />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<h2>Page not found</h2>} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
